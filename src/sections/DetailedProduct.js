c9f0a75d1be9a02c1b58a9cc180a085bfd9ef63f
Lucas Nykvist <lucasnykvist01@gmail.com>
2022-11-11 16:44:13 +0100

    working on detailed products view

--- src/sections/DetailedProduct.js
+++ src/sections/DetailedProduct.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Breadcrumb from '../components/Breadcrumb';
@@ -6,6 +6,32 @@ import { productContext } from '../contexts/contexts';
 import { useShoppingCart } from '../contexts/ShoppingCartContext';
 
 const DetailedProduct = () => {
+
+    const menuData = [
+        {
+            title: "Description"
+        },
+        {
+            title: "Additional"
+        },
+        {
+            title: "Shopping & Returns "
+        },
+        {
+            title: "Reviews"
+        }
+    ]
+
+    const [quantity, setQuantity] = useState(0);
+
+    const increment = () => {
+        setQuantity(quantity + 1)
+    }
+    const decrement = () => {
+        if (quantity >= 1) {
+            setQuantity(quantity - 1)
+        }
+    }
 
     const { incrementQuantity } = useShoppingCart()
     const products = useContext(productContext)
@@ -68,11 +94,23 @@ const DetailedProduct = () => {
                                     <div className="quantity">
                                         <p>Qty:</p>
                                         <div className="amount">
-                                            <button className="decrement">-</button>
-                                            <div className="current">1</div>
-                                            <button className="increment">+</button>
+                                            <button onClick={decrement} className="decrement">-</button>
+                                            <div className="current">{quantity}</div>
+                                            <button onClick={increment} className="increment">+</button>
                                         </div>
                                         <button onClick={() => incrementQuantity({ articleNumber: detailed.articleNumber, product: detailed })}>ADD TO CART</button>
+                                    </div>
+                                    <div className="share">
+                                        <p>Share:</p>
+                                        <div className="share-links">
+                                            <ul>
+                                                <li><i class="fa-brands fa-facebook-f"></i></li>
+                                                <li><i class="fa-brands fa-instagram"></i></li>
+                                                <li><i class="fa-brands fa-twitter"></i></li>
+                                                <li><i class="fa-brands fa-google"></i></li>
+                                                <li><i class="fa-brands fa-linkedin"></i></li>
+                                            </ul>
+                                        </div>
                                     </div>
 
                                 </div>
@@ -82,6 +120,30 @@ const DetailedProduct = () => {
                     </div>
                 ))
             }
+
+            <div className="desc-menu">
+                <div className="container">
+                    <div className="menu">
+                        {
+                            menuData.map(({ title }) => {
+                                return (
+                                    <button className='menu-button'>{title}</button>
+                                )
+                            })
+                        }
+                    </div>
+
+                    <div className="info">
+                        <p>Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard. </p>
+                        <ul>
+                            <li>* Village did removed enjoyed explain nor ham saw calling talking.</li>
+                            <li>* Securing as informed declared or margaret.</li>
+                            <li>* Joy horrible moreover man feelings own shy.</li>
+                        </ul>
+                        <p>On even feet time have an no at. Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. Always get adieus nature day course for common. My little garret repair to desire he esteem. </p>
+                    </div>
+                </div>
+            </div>
         </>
 
 
